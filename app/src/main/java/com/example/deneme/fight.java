926903d34a9a00d77345f2f3e0d6449dad615517
voidassasin <56587556+voidassasin@users.noreply.github.com>
2019-12-02 17:33:59 +0300

    Create fight.java

---
+++ app/src/main/java/com/example/deneme/fight.java
@@ -0,0 +1,14 @@
+package com.example.deneme;
+
+import android.app.Activity;
+
+public class fight {
+
+    public void fight(WarriorStat warrior, Object enemy , int level)
+    {
+        if(enemy.getClass()==goblin.class)
+        {
+
+        }
+    }
+}
